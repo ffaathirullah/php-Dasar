e6b4c286ff108e4b0618fab2ee444013271f3603
Fachrul_Faathirullah <lenpuchongloi93@gmail.com>
2021-02-05 01:39:33 +0700

    get & post 3

--- enam/login/admin.php
+++ enam/login/admin.php
@@ -5,6 +5,8 @@
     <title>Selamat Datang Admin</title>
   </head>
   <body>
-<h1></h1>
+<h1>Selamat Datang Admin</h1>
+
+<a href="login.php">Logout</a>
   </body>
 </html>
--- enam/login/login.php
+++ enam/login/login.php
@@ -1,10 +1,34 @@
+<?php
+//apakah tombol sudah ditekan
+if (isset($_POST["submit"])) {
+  if ($_POST["username"] == "admin" && $_POST["password"] = "12345") {
+    header("Location: admin.php");
+  }else {
+    $error = true;
+  }
+}
+ ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Login Admin</title>
+    <?php if (isset($error)) : ?>
+        <p>Username atau Password Salah</p>
+    <?php endif; ?>
   </head>
   <body>
-
+    <h1>Login Admin</h1>
+    <ul>
+      <form action="" method="post">
+        <li><label for="username">Username :</label>
+        <input type="text" name="username" id="username">
+        </li>
+        <li><label for="password">Password :</label>
+        <input type="password" name="password" id="password">
+        </li>
+        <button type="submit" name="submit">Login</button>
+      </form>
+    </ul>
   </body>
 </html>
